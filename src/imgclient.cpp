f0fd98d81ef94850c3eb6f8f71d9a7ee60e9fe46
ltaparkrunner <ltaparkrunner@gmail.com>
2026-04-16 02:28:16 +0400

    The implementation use oneof method to differ protobuf messages. It successfully transmits data.

--- src/imgclient.cpp
+++ src/imgclient.cpp
@@ -74,14 +74,17 @@ void ImageClient::sendImage(const QString &filePath) {
     // [Здесь должна быть сериализация через сгенерированный Protobuf класс]
     // Для теста отправим просто байты:
 
-    pict_data::ImageUpload message;
+    pict_data::BaseMessage base;
+    pict_data::Picture message;
     message.setFilename(fileInfo.fileName());
     message.setEmailLogin("forever_young");
     message.setData(fileData.constData());
-    message.setContentType("image");
+    message.setContenttype("image_1");
     message.setTimestamp(QDateTime::currentMSecsSinceEpoch());
+
+    base.setPict(message);
     QProtobufSerializer serializer;
-    QByteArray data = message.serialize(&serializer);
+    QByteArray data = base.serialize(&serializer);
     qint64 sz = m_webSocket.sendBinaryMessage(data);
 
     qDebug() << "Image sent, size:" << sz;
@@ -126,54 +129,3 @@ void ImageClient::setupSslConfiguration() {
         m_webSocket.setSslConfiguration(config);
     }
 }
-
-/*
-
-to use inside method sendImage
-
-
-#include "client.h"
-#include <QDebug>
-
-ImageClient::ImageClient(QObject *parent) : QObject(parent) {
-    connect(&m_webSocket, &QWebSocket::connected, this, &ImageClient::onConnected);
-    connect(&m_webSocket, &QWebSocket::binaryMessageReceived, this, &ImageClient::onBinaryMessageReceived);
-}
-
-void ImageClient::connectToServer(const QUrl &url) {
-    m_webSocket.open(url);
-}
-
-void ImageClient::onConnected() {
-    qDebug() << "Connected to server!";
-}
-
-void ImageClient::sendImage(const QString &filePath) {
-    QFile file(filePath);
-    if (!file.open(QIODevice::ReadOnly)) {
-        qDebug() << "Cannot open file";
-        return;
-    }
-
-    QByteArray fileData = file.readAll();
-
-    // ВАЖНО: Если сервер ждет Socket.io пакет, нужно добавить префикс.
-    // Если сервер ждет чистый Protobuf через сырой WebSocket:
-    // [Здесь должна быть сериализация через сгенерированный Protobuf класс]
-    // Для теста отправим просто байты:
-    m_webSocket.sendBinaryMessage(fileData);
-
-    qDebug() << "Image sent, size:" << fileData.size();
-}
-
-void ImageClient::onBinaryMessageReceived(const QByteArray &message) {
-    qDebug() << "Answer from server received";
-}
-
-
-3. Нюанс для Qt (C++)
-Теперь в вашем Qt-коде метод sendImage будет работать напрямую:
-Используйте m_webSocket.sendBinaryMessage(serializedData).
-URL для подключения в Qt будет: ws://localhost:3000.
-
-*/
